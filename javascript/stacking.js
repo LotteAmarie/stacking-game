79f5d6de86e15f6cd78e78507f9b4a30f0dc0d1a
Josh Theisen <jtheisen93@gmail.com>
2019-02-22 07:16:25 -0600

    First implementation of json data as well as JSDoc

--- javascript/stacking.js
+++ javascript/stacking.js
@@ -290,6 +290,31 @@ function clearMatches(matches) {
     });
 }
 
+/**
+ * Function to utilize data from a local JSON file asynchronously.
+ * 
+ * Reference used: https://codepen.io/KryptoniteDove/post/load-json-file-locally-using-pure-javascript
+ * @param {Function} callback 
+ */
+function loadJSON(callback) {   
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', 'javascript/stacking-config.json', true); 
+    xobj.onreadystatechange = function () {
+        if (xobj.readyState == 4 && xobj.status == "200") {
+            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            callback(xobj.responseText);
+        }
+    };
+    xobj.send(null);  
+}
+
+/**
+ * Function to create the initial state of the 2d game board.
+ *  
+ * @param {Number} cols number of columns
+ * @param {Number} rows number of rows
+ */
 function createBoard(cols, rows) {
     var arr = new Array(cols).fill(null);
 
